66b3242734b6c36f1c18beaff1d0ee7af25de8e1
ymnavfka <60478644+ymnavfka@users.noreply.github.com>
2020-11-29 18:50:15 +0300

    Add files via upload

---
+++ MND_Task_07/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace MND_Task_07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int arraySize = 0;
+            do
+            {
+                Console.Write("Enter the size of array: ");
+
+                if (!int.TryParse(Console.ReadLine(), out arraySize))
+                    Console.WriteLine("\nYou need to enter numbers!");
+                else if (arraySize <= 0)
+                    Console.WriteLine("The array size cannot be negative or equal to zero.");
+                else
+                    break;
+            } while (true);
+
+            int[] array = new int[arraySize];
+            Random rnd = new Random();
+            for (int i = 0; i < arraySize; i++) array[i] = rnd.Next(0, 100);
+
+            Console.WriteLine("\nOriginal array:");
+            for (int i = 0; i < arraySize; i++) Console.Write(array[i] + " ");
+            Console.WriteLine("\n\nThe resulting array:");
+            Shuffle(array);
+            for (int i = 0; i < arraySize; i++) Console.Write(array[i] + " ");
+            Console.ReadKey();
+        }
+        static void Shuffle(int[] array)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                int rndSelect = rnd.Next(0, i + 1);
+                int j = array[rndSelect];
+                array[rndSelect] = array[i];
+                array[i] = j;
+            }
+        }
+    }
+}
---
+++ MND_Task_07/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("MND_Task_07")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MND_Task_07")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("c1195559-a43c-4268-83af-1541ae8b20b0")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
